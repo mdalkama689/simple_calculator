9c8cf7159380f955139afa065c6f9f9f198bcff1
mdalkama <mdalkama689@gmail.com>
2025-05-10 11:30:09 +0530

    add info icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Calculator from './components/Calculator/Calculator';
+import MobileShare from './components/share/MobileShare';
+import TabShare from './components/share/TabShare';
 
 function App() {
+    const [deviceType, setDeviceType] = useState<"big" | "small">("big");
+
+
+  useEffect(() => {
+    const handleResize = () => {
+        setDeviceType(window.innerWidth < 1024 ? "small" : "big");
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="min-h-screen bg-[#121212] flex items-center justify-center p-4">
+    {deviceType === "small" ? <MobileShare /> : <TabShare />}
       <Calculator />
     </div>
   );
